dd67295b675717a8311dfcf665a24dfc5fd58bd9
Jihong Park <ggikko@hotmail.com>
2016-04-25 12:51:15 +0900

    add main layout

--- SwipeTableLayoutVersionTwo/app/src/main/java/ggikko/me/swipetablelayoutversiontwo/MainActivity.java
+++ SwipeTableLayoutVersionTwo/app/src/main/java/ggikko/me/swipetablelayoutversiontwo/MainActivity.java
@@ -3,11 +3,17 @@ package ggikko.me.swipetablelayoutversiontwo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** inject view */
+        ButterKnife.bind(this);
+
     }
 }
